a47e1159a6598d2b3cf6e4945d83c2c240e74f5a
Макаров Евгений <you@example.com>
2025-04-19 10:16:14 +0300

    second attempt

--- parcel_test.go
+++ parcel_test.go
@@ -164,7 +164,7 @@ func TestGetByClient(t *testing.T) {
 	for _, parcel := range storedParcels {
 		// в parcelMap лежат добавленные посылки, ключ - идентификатор посылки, значение - сама посылка
 		// убедитесь, что все посылки из storedParcels есть в parcelMap
-		require.Contains(t, parcelMap, parcel.Number)
+		assert.Contains(t, parcelMap, parcel.Number)
 		// убедитесь, что значения полей полученных посылок заполнены верно
 		assert.Equal(t, parcel, parcelMap[parcel.Number])
 
